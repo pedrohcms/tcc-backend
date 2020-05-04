ffccbb6dc87750f8354002eefe0423cd8cfa6910
Pedro Henrique Correa Mota da Silva <pedrohcms2018@gmail.com>
2020-05-04 11:08:52 -0300

    Added database configuration for tests and jest configuration file

--- src/config/database.js
+++ src/config/database.js
@@ -15,6 +15,19 @@ module.exports = {
       underscored_all: true,
     },
   },
-  test: {},
+  test: {
+    username: env.TEST_DB_USERNAME,
+    password: env.TEST_DB_PASSWORD,
+    database: env.TEST_DB_NAME,
+    host: env.TEST_DB_HOST,
+    port: env.TEST_DB_PORT,
+    dialect: env.TEST_DB_DIALECT,
+    operatorsAliases: 0,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscored_all: true,
+    },
+  },
   production: {},
 };
