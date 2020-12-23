c96f2f64cb9bd9bddeeab672affdd84951038d80
pedrohcms <pedrohcms2018@gmail.com>
2020-12-22 23:44:57 -0300

    Adicionando porta

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ config();
 
 import express from "./App";
 
-express.listen(process.env.PORT || 3333, () => console.log('API online'));
+express.listen(process.env.PORT || 3333, () => console.log('API online na porta: ' + process.env.PORT || 3333));
